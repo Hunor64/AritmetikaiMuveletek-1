0a4b93cae24ef3573434a792227f027d5696d1ac
hunor64 <nhunor06@gmail.com>
2024-09-05 13:57:26 +0200

    done

--- AritmetikaiMuveletek-1/Program.cs
+++ AritmetikaiMuveletek-1/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int sorokSzama = int.Parse(Console.ReadLine());
+List<double> eredmenyek = new();
+for (int i = 0; i < sorokSzama; i++)
+{
+    string sor = Console.ReadLine();
+    var elemek = sor.Split(' ');
+    if (elemek[1] == "+")
+    {
+        eredmenyek.Add(int.Parse(elemek[0]) + int.Parse(elemek[2]));
+    }
+    else if (elemek[1] == "-")
+    {
+        eredmenyek.Add(int.Parse(elemek[0]) - int.Parse(elemek[2]));
+    }
+    else if (elemek[1] == "*")
+    {
+        eredmenyek.Add(int.Parse(elemek[0]) * int.Parse(elemek[2]));
+    }
+    else if (elemek[1] == "/")
+    {
+        eredmenyek.Add(int.Parse(elemek[0]) / int.Parse(elemek[2]));
+    }
+    else if (elemek[1] == "%")
+    {
+        eredmenyek.Add(int.Parse(elemek[0]) % int.Parse(elemek[2]));
+    }
+}
+foreach (var item in eredmenyek)
+{
+    Console.WriteLine(item);
+}
